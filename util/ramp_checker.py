774cedb97559dbb0a69d10e961550823845d4d8b
Nirav <niravsurajlal@gmail.com>
2024-09-10 11:41:14 +0200

    moved ramp checks to new file and allowed possibility of diffferent types of rampt checks

---
+++ util/ramp_checker.py
@@ -0,0 +1,90 @@
+"""_summary_
+
+    Args:
+        card_details (dict): _description_
+        ramp_checks (dict): _description_
+        permanent_types (list): _description_
+
+    Returns:
+        float: _description_
+
+    - SolRing - score 2.0
+    - Dimir Signet - score 1.12
+    """
+
+
+import re
+
+class RampChecker:
+    def __init__ (self, card_details: dict, json_tag_description: dict, \
+            is_card_perm: bool, cardname: str):
+
+        self.card_details: dict = card_details
+        self.json_tag_description: dict = json_tag_description
+        self.is_card_perm: bool = is_card_perm
+        self.cardname: str = cardname
+        self.avg_ramp_weight: float = 0.0
+        self.card_text: str = self.card_details['text']
+
+    def assign_score(self):
+        json_text_flags = self.json_tag_description['card_description_flags']
+
+        if self.is_card_perm:
+            for flag_desc, normal_M_to_CC in json_text_flags['perm'].items():
+                flags_desc_updated =  r'(.*?)'.join(flag_desc.split('&'))
+                print(flags_desc_updated)
+                if re.search(flags_desc_updated, self.card_text):
+                    self.__select_ramp_function__(flag_desc)
+                # elif True:
+                #     print(f"Unchecked ramp text:\n{self.card_text}")
+
+        else:
+            pass
+
+        return 0
+
+    def __select_ramp_function__(self, flag_description):
+        match flag_description:
+            case "{T}&Add":
+                self.tap_to_add()
+            case "create&treasure":
+                pass
+            case "search&land":
+                pass
+            case "additional&land":
+                pass
+            case "cost&less":
+                self.cost_less()
+            case "put&land":
+                pass
+            case "!when&cast":
+                pass
+            case "!when&enters|leaves&battelfield":
+                pass
+            case _:
+                print("None to match")
+
+    def tap_to_add(self):
+        num_colours = 0
+        colour_fixing_total = 0
+        taps_for = re.findall(r"{[C, B, U, R, G, W]}", self.card_text)
+        mana_to_cc_ratio = len(taps_for)/self.card_details['manaValue']
+
+        for color in ['{B}', '{U}', '{R}', '{G}', '{W}']:
+            num_of_coloured_mana = taps_for.count(color)
+            if num_of_coloured_mana > 0:
+                num_colours = num_colours + 1
+            colour_fixing_total = colour_fixing_total + num_of_coloured_mana
+
+        colour_fixing_weight = 1 + num_colours/50 + colour_fixing_total/25
+
+        self.avg_ramp_weight = self.avg_ramp_weight + mana_to_cc_ratio*colour_fixing_weight
+
+    def cost_less(self):
+        num_colours = 0
+        colour_fixing_total = 0
+        mana_reduction_colour = re.findall(r"{[C, B, U, R, G, W]}", self.card_text)
+        mana_reduction = re.findall(r"{[0-9]}", self.card_text)
+
+
+        pass
--- util/update_my_flags.py
+++ util/update_my_flags.py
@@ -6,12 +6,14 @@ import os
 import json
 import re
 
+from ramp_checker import RampChecker
+
 __MY_CARDLIST_LOCATION__ = os.path.join('data', 'my_cardlist.json')
 __FLAGS_UPDATE_LOCATION__ = os.path.join('data', 'mtg_defs.json')
 __DEBUG__ = True
 
 def check_if_card_is_perm(card_types: list, permenant_types: list) -> bool:
-    """_summary_
+    """Checks if a card is of the permenant type
 
     Args:
         card_types (list): _description_
@@ -25,51 +27,10 @@ def check_if_card_is_perm(card_types: list, permenant_types: list) -> bool:
                 return True
     return False
 
-def ramp_check(card_details: dict, tag_description: dict, \
-    permanent_types: list, cardlist) -> float:
-    """_summary_
-
-    Args:
-        card_details (dict): _description_
-        ramp_checks (dict): _description_
-        permanent_types (list): _description_
-
-    Returns:
-        float: _description_
-    """
-    permanent_types.remove('Land')
-
-    if __DEBUG__:
-        card_name = 'Undead Warchief'
-        card_details = cardlist[card_name]
-
-    text_flags = tag_description['card_description_flags']
-    card_text = card_details['text']
-    card_perm = check_if_card_is_perm(card_details['types'], permanent_types)
-
-    avg_ramp_weight = 0
-
-    if card_perm:
-        for flag_desc, normal_M_to_CC in text_flags['perm'].items():
-            flags =  r'(.*?)'.join(flag_desc.split('&'))
-            print(flags)
-            if re.search(flags, card_text):
-                # tap to add
-                taps_for = re.findall(r"{[C, B, U, R, G, W]}", card_text)
-                mana_to_cc_ratio = len(taps_for)/card_details['manaValue']
-                avg_ramp_weight = avg_ramp_weight + mana_to_cc_ratio
-
-            elif re.search(flags, card_text):
-                print(f"Unchecked ramp text:\n\t{card_text}")
-
-    else:
-        pass
-
-    return 0
-
 
 def update_card_list_flags():
-    """_summary_
+    """ Updates a cards flags to be used by us.
+        These will be used to srot and give scores to cards.
     """
     with open(__MY_CARDLIST_LOCATION__, 'r', encoding='utf-8') as f:
         cardlist = json.load(f)
@@ -81,15 +42,25 @@ def update_card_list_flags():
 
     for card_name,card_details in cardlist.items():
 
+        if __DEBUG__:
+            card_name = 'Undead Warchief'
+            # card_name = 'Sol Ring'
+            card_details = cardlist[card_name]
 
-        for tag,tag_description in flags_list['tags'].items():
-            if tag == 'ramp':
+        permanent_types_copy = permanent_types.copy()
+        permanent_types_copy.remove('Land')
+        is_card_perm = check_if_card_is_perm(card_details['types'], permanent_types_copy)
 
-                ramp_score = ramp_check(card_details, tag_description,
-                                        permanent_types.copy(), cardlist)
+        for json_tag,json_tag_description in flags_list['tags'].items():
+            if json_tag == 'ramp':
+
+                # ramp_score = ramp_check(card_details, tag_description,
+                #                         is_card_perm)
+                r = RampChecker(card_details, json_tag_description, is_card_perm, card_name)
+                r.assign_score()
+                print(f"Card {card_name} | score {r.avg_ramp_weight}|")
 
     print('here')
-
 
 if __name__ == "__main__":
     update_card_list_flags()
